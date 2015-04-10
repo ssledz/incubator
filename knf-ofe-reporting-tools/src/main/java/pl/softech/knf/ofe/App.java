424516cbb60b28ce27ca18909e37940159e5bdbb
ssledz <slawomir.sledz@gmail.com>
2015-04-10 20:28:48 +0200

    Initial import

---
+++ knf-ofe-reporting-tools/src/main/java/pl/softech/knf/ofe/App.java
@@ -0,0 +1,13 @@
+package pl.softech.knf.ofe;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	public static void main(final String[] args) {
+		LOGGER.info("Starting...");
+	}
+}
